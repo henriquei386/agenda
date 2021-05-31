7e751187bd06f7802b43f94f9659fa109ed8b1d0
henriquei386 <henriquei386@outlook.com.br>
2021-05-31 11:40:23 -0300

    Projeto iniciado

---
+++ android/app/src/main/kotlin/br/com/henrique/agenda/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.henrique.agenda
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
