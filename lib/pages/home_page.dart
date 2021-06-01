a6c6b8792d0d0c6bd83640620c1ba0ebc9721c6a
henriquei386 <henriquei386@outlook.com.br>
2021-06-01 09:38:59 -0300

    Card criado

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import '../helper/contact_helper.dart';
 
@@ -21,6 +23,62 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  // Contatos
+  Widget _contactCard(BuildContext context, int index) {
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: contacts[index].image != null
+                        ? FileImage(
+                            File(contacts[index].image!),
+                          )
+                        : AssetImage('assets/images/user.png') as ImageProvider,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      contacts[index].name!,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      contacts[index].email!,
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      contacts[index].phone!,
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +90,7 @@ class _HomePageState extends State<HomePage> {
         padding: EdgeInsets.all(10.0),
         itemCount: contacts.length,
         itemBuilder: (BuildContext context, int index) {
-          return Container();
+          return _contactCard(context, index);
         },
       ),
       floatingActionButton: FloatingActionButton(
